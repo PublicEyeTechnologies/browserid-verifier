140df7d3c50fa62eec0c04f6fb853213c295bd9d
Jonathan Flowers <jonny_flowers@me.com>
2015-12-18 15:33:52 +0000

    initial commit

---
+++ tests/zzz-report-coverage.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global describe,it,require */
+
+const EXPECT_COVERAGE = 95;
+
+if (!process.env.NO_COVERAGE) {
+  var
+  should = require('should'),
+  ass = require('ass'),
+  fs = require('fs');
+
+  describe('code coverage', function() {
+    it('tests should exceed ' + EXPECT_COVERAGE + '% coverage', function(done) {
+      ass.report('json', function(err, r) {
+        should.not.exist(err);
+        (r.percent).should.be.above(EXPECT_COVERAGE);
+        done();
+      });
+    });
+
+    it('coverage.html should be written', function(done) {
+      ass.report('html', function(err, html) {
+        should.not.exist(err);
+        fs.writeFileSync('coverage.html', html);
+        done();
+      });
+    });
+  });
+}
